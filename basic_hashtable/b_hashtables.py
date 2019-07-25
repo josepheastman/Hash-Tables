248b87f7d47a2fc9e0d6374f9c652687265b9791
Joseph <josephryan96@gmail.com>
2019-07-24 20:42:41 -0700

    work on hash tables

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,7 +15,8 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * capacity
 
 
 # '''
@@ -23,7 +24,10 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 5381
+    for x in string:
+        hash = ((hash << 5) + hash) + ord(x)
+    return hash % max
 
 
 # '''
@@ -31,8 +35,24 @@ def hash(string, max):
 
 # If you are overwriting a value with a different key, print a warning.
 # '''
+
 def hash_table_insert(hash_table, key, value):
-    pass
+    index = hash(key, hash_table.capacity)
+    print(index)
+
+    pair = Pair(key, value)
+
+    if hash_table.storage[index] is not None:
+        print("Warning: overwriting ") + str(hash_table.storage[index])
+    hash_table.storage[index] = pair
+
+# def hash_table_insert(hash_table, key, value):
+#     index = hash(key, hash_table.capacity)
+#     print(index)
+
+#     if hash_table.storage[index] is not None:
+#         print("Warning: overwriting ") + str(hash_table.storage[index])
+#     hash_table.storage[index] = value
 
 
 # '''
@@ -51,6 +71,11 @@ def hash_table_remove(hash_table, key):
 # '''
 def hash_table_retrieve(hash_table, key):
     pass
+    # for x in hash_table:
+    #     if x[0] == key:
+    #         return x[1]
+    #     else:
+    #         return None
 
 
 def Testing():
