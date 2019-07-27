0d411240ef72af522b74efcfbbede80550b190c0
Joseph <josephryan96@gmail.com>
2019-07-27 08:43:01 -0700

    arrays.py

--- array_linked_list.py/arrays.py
+++ array_linked_list.py/arrays.py
@@ -4,24 +4,36 @@
 class array:
     def __init__(self):
         # Your code here
-        pass
+        self.capacity = capacity
+        self.count = 0
+        self.elements = [None] * capacity
 
 
 # Double the size of the given array
 def resize_array():
     # Your code here
-    pass
+    new_capacity = array.capacity * 2
+    new_elements = [None] * new_capacity
+
+    for i in range(array.capacity):
+        new_elements[i] = array.elements[i]
+
+    array.elements = new_elements
+    array.capacity = new_capacity
 
 
 # Return an element of a given array at a given index
-def array_read():
+def array_read(array, index):
     # Throw an error if array is out of the current count
-    # Your code here
-    pass
+    if index >= array.count:
+        print("Error! index " + str(index) + " out of range")
+        return None
+
+    return array.elements[index]
 
 
 # Insert an element in a given array at a given index
-def array_insert():
+def array_insert(array, value, index):
     # Throw an error if array is out of the current count
 
     # Resize the array if the number of elements is over capacity
@@ -30,32 +42,66 @@ def array_insert():
     # Think about where to start!
 
     # Add the new element to the array and update the count
-    pass
+    if index > array.count:
+        print("Error, index " + str(index) + " out of range")
+        return None
+
+    if array.capacity <= array.count:
+        resize_array(array)
+
+    for i in range(array.count, index, -1):
+        array.elements[i] = array.elements[i-1]
+
+    array.elements[index] = value
+    array.count += 1
 
 
 # Add an element to the end of the given array
-def array_append():
+def array_append(array, value):
 
     # Hint, this can be done with one line of code
     # (Without using a built in function)
 
     # Your code here
-    pass
+    array_insert(array, value, array.count)
 
 
 # Remove the first occurence of the given element from the array
 # Throw an error if the value is not found
-def array_remove():
+def array_remove(array, element):
     # Your code here
-    pass
+    removed = False
+    for i in range(array.count):
+        if removed:
+            array.elements[i-1] = array.elements[i]
+        elif array.elements[i] == element:
+            removed = True
+
+    if removed:
+        array.count -= 1
+        array.elements[array.count] = None
+    else:
+        print("Error, element" + str(element) + " not found")
 
 
 # Remove the element in a given position and return it
 # Then shift every element after that occurrance to fill the gap
-def array_pop():
+def array_pop(array, index):
     # Throw an error if array is out of the current count
     # Your code here
-    pass
+    if index > array.count:
+        print("Error, index " + str(index) + " is out of range")
+        return None
+
+    return_value = array.elements[index]
+
+    for i in range(index + 1, array.count):
+        array.elements[i - 1] = array.elements[i]
+
+    array.count -= 1
+    array.elements[array.count] = None
+
+    return return_value
 
 
 # Utility to print an array
